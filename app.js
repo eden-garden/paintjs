136f3ff74c9415c179fb97dce04c1a5ce7d4f7da
Eden <63891593+eden-garden@users.noreply.github.com>
2022-03-27 20:54:49 +0900

    Eraser : how can I make it?
    I don't just cover the drawings by coloring white. it's just only second color. I want REMOVE the PIXEL ITSELF! But I still have no way. I will figure out it tomorrow

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
+const eraserBtn = document.getElementById("jsEraser");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
+
 
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
@@ -39,6 +41,8 @@ function onMouseMove(event) {
     ctx.stroke();
   }
 }
+
+
 
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
@@ -91,6 +95,10 @@ if (canvas) {
 Array.from(colors).forEach(color =>
   color.addEventListener("click", handleColorClick)
 );
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
 
 if (range) {
   range.addEventListener("input", handleRangeChange);
